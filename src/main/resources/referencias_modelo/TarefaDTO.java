173a5994dcd0c244cb597e2335e1288a99d1bcb5
digocoutinho78 <digocoutinho@outlook.com>
2024-06-12 15:30:44 -0300

    pasta referencias_modelo e documentacao

---
+++ src/main/resources/referencias_modelo/TarefaDTO.java
@@ -0,0 +1,16 @@
+package com.br.serratec.colaborattor.dto;
+
+public class TarefaDTO {
+    private String nome;
+    private String descricao;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private String status;
+    private Double esforcoEstimado;
+    private Double esforcoReal;
+    private Long idResponsavel;
+    private Long idProjeto;
+    private Long idFerramentaALM;
+
+    // Getters e Setters
+}
