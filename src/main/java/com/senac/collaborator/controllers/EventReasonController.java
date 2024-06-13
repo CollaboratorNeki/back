a928b3e6ea92971ba8617a1ccff42d9b331f6c03
digocoutinho78 <digocoutinho@outlook.com>
2024-06-13 16:40:48 -0300

    init eventReason controller e service - parcial

--- src/main/java/com/senac/collaborator/controllers/EventReasonController.java
+++ src/main/java/com/senac/collaborator/controllers/EventReasonController.java
@@ -1,5 +1,47 @@
 package com.senac.collaborator.controllers;
 
-public class EventReasonController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.senac.collaborator.model.EventReason;
+import com.senac.collaborator.services.EventReasonService;
+
+@RestController
+@RequestMapping("/api/event-reasons")
+public class EventReasonController {
+	@Autowired
+	private EventReasonService eventReasonService;
+
+	@GetMapping
+	public List<EventReason> getAllEventReasons() {
+		return eventReasonService.getAllEventReasons();
+	}
+
+//	@GetMapping("/{id}")
+//	public EventReason getEventReasonById(@PathVariable Long id) {
+//		return eventReasonService.getEventReasonById(id);
+//	}
+
+	@PostMapping
+	public EventReason createEventReason(@RequestBody EventReason eventReason) {
+		return eventReasonService.createEventReason(eventReason);
+	}
+
+//	@PutMapping("/{id}")
+//	public EventReason updateEventReason(@PathVariable Long id, @RequestBody EventReason eventReason) {
+//		return eventReasonService.updateEventReason(id, eventReason);
+//	}
+
+	@DeleteMapping("/{id}")
+	public void deleteEventReason(@PathVariable Long id) {
+		eventReasonService.deleteEventReason(id);
+	}
 }
---
+++ src/main/java/com/senac/collaborator/services/EventReasonService.java
@@ -0,0 +1,42 @@
+package com.senac.collaborator.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.senac.collaborator.model.EventReason;
+import com.senac.collaborator.repositores.EventReasonRepository;
+
+@Service
+
+public class EventReasonService {
+
+	@Autowired
+	private EventReasonRepository eventReasonRepository;
+
+	public List<EventReason> getAllEventReasons() {
+		return eventReasonRepository.findAll();
+	}
+
+//	public EventReason getEventReasonById(Long id) {
+//		return eventReasonRepository.findById(id)
+//				.orElseThrow(() -> new ResourceNotFoundException("EventReason not found"));
+//	}
+
+	public EventReason createEventReason(EventReason eventReason) {
+		return eventReasonRepository.save(eventReason);
+	}
+
+//	public EventReason updateEventReason(Long id, EventReason eventReasonDetails) {
+//		EventReason eventReason = eventReasonRepository.findById(id)
+//				.orElseThrow(() -> new ResourceNotFoundException("EventReason not found"));
+//		eventReason.setNome(eventReasonDetails.getNome());
+//		eventReason.setDescricao(eventReasonDetails.getDescricao());
+//		return eventReasonRepository.save(eventReason);
+//	}
+
+	public void deleteEventReason(Long id) {
+		eventReasonRepository.deleteById(id);
+	}
+}
