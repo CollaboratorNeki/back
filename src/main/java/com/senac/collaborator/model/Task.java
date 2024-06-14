df2b1f3f118e133f763b46b56e48ad79a00aa0e3
MatheusMelloDev <165337919+MatheusMelloDev@users.noreply.github.com>
2024-06-14 11:43:25 -0300

    alterando entidade task

--- src/main/java/com/senac/collaborator/model/Task.java
+++ src/main/java/com/senac/collaborator/model/Task.java
@@ -7,155 +7,161 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import java.time.LocalDate;
+import java.util.List;
 
 @Entity
-@Table(name = "Task")
+@Table(name = "task")
 public class Task {
 
-    @Id  
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name ="idTask")
-    private long idTask;
-    
-    @Column(name ="nome")
-    private String nome;
-    
-    @Column(name ="descricao")
-    private String descricao;
-    
-    @Column(name ="dataInicio")
-    private LocalDate dataInicio;
-    
-    @Column(name ="dataFim")
-    private LocalDate dataFim;
-    
-    @Column(name ="status")
-    private boolean status;
-    
-    @Column(name ="esforcoEstimado")
-    private String esforcoEstimado;
-    
-    @Column(name ="esforcoReal")
-    private String esforcoReal;
-    
-    @ManyToOne
-    @JoinColumn(name = "idResponsavel")
-    private User idResponsavel;
-    
-    @ManyToOne
-    @JoinColumn(name = "idProject")
-    private Project idProject;
-    
-    @ManyToOne
-    @JoinColumn(name = "idALMTool")
-    private ALMTool idalmtool;
-    
-    public Task() {}
+	@Id  
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name ="id_task")
+	private long id_task;
+	
+	@Column(name ="nome")
+	private String nome;
+	
+	@Column(name ="descricao")
+	private String descricao;
+	
+	@Column(name ="data_inicio")
+	private int data_inicio;
+	
+	@Column(name ="dat_fim")
+	private int data_fim;
+	
+	@Column(name ="status")
+	private String status;
+	
+	@Column(name ="esforco_estimado")
+	private String esforco_estimado;
+	
+	@Column(name ="esforco_real")
+	private String esforco_real;
+	
 
-    public Task(long idTask, String nome, String descricao, LocalDate dataInicio, LocalDate dataFim, boolean status,
-            String esforcoEstimado, String esforcoReal, User idResponsavel, Project idProject, ALMTool idalmtool) {
-        this.idTask = idTask;
-        this.nome = nome;
-        this.descricao = descricao;
-        this.dataInicio = dataInicio;
-        this.dataFim = dataFim;
-        this.status = status;
-        this.esforcoEstimado = esforcoEstimado;
-        this.esforcoReal = esforcoReal;
-        this.idResponsavel = idResponsavel;
-        this.idProject = idProject;
-        this.idalmtool = idalmtool;
-    }
+	@ManyToOne
+	@JoinColumn(name = "idResponsavel")
+	private User  idResponsavel;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "id_project")
+	private Project project;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "id_almtool")
+	private ALMTool id_almtool;
+	
+	@OneToMany
+	(mappedBy="id_task")
+	private List<Task> id_taskList;
+	
+	
+	public Task () {}
 
-    // Getters and Setters
 
-    public long getIdTask() {
-        return idTask;
-    }
+	public Task(long id_task, String nome, String descricao, int data_inicio, int data_fim, String status,
+			String esforco_estimado, String esforco_real) {
+		super();
+		this.id_task = id_task;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.data_inicio = data_inicio;
+		this.data_fim = data_fim;
+		this.status = status;
+		this.esforco_estimado = esforco_estimado;
+		this.esforco_real = esforco_real;
+	}
 
-    public void setIdTask(long idTask) {
-        this.idTask = idTask;
-    }
 
-    public String getNome() {
-        return nome;
-    }
+	public long getId_task() {
+		return id_task;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 
-    public String getDescricao() {
-        return descricao;
-    }
+	public void setId_task(long id_task) {
+		this.id_task = id_task;
+	}
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
-    public LocalDate getDataInicio() {
-        return dataInicio;
-    }
+	public String getNome() {
+		return nome;
+	}
 
-    public void setDataInicio(LocalDate dataInicio) {
-        this.dataInicio = dataInicio;
-    }
 
-    public LocalDate getDataFim() {
-        return dataFim;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public void setDataFim(LocalDate dataFim) {
-        this.dataFim = dataFim;
-    }
 
-    public boolean isStatus() {
-        return status;
-    }
+	public String getDescricao() {
+		return descricao;
+	}
 
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
 
-    public String getEsforcoEstimado() {
-        return esforcoEstimado;
-    }
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
-    public void setEsforcoEstimado(String esforcoEstimado) {
-        this.esforcoEstimado = esforcoEstimado;
-    }
 
-    public String getEsforcoReal() {
-        return esforcoReal;
-    }
+	public int getData_inicio() {
+		return data_inicio;
+	}
 
-    public void setEsforcoReal(String esforcoReal) {
-        this.esforcoReal = esforcoReal;
-    }
 
-    public User getIdResponsavel() {
-        return idResponsavel;
-    }
+	public void setData_inicio(int data_inicio) {
+		this.data_inicio = data_inicio;
+	}
 
-    public void setIdResponsavel(User idResponsavel) {
-        this.idResponsavel = idResponsavel;
-    }
 
-    public Project getIdProject() {
-        return idProject;
-    }
+	public int getData_fim() {
+		return data_fim;
+	}
 
-    public void setIdProject(Project idProject) {
-        this.idProject = idProject;
-    }
 
-    public ALMTool getIdalmtool() {
-        return idalmtool;
-    }
+	public void setData_fim(int data_fim) {
+		this.data_fim = data_fim;
+	}
 
-    public void setIdalmtool(ALMTool idalmtool) {
-        this.idalmtool = idalmtool;
-    }
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	public String getEsforco_estimado() {
+		return esforco_estimado;
+	}
+
+
+	public void setEsforco_estimado(String esforco_estimado) {
+		this.esforco_estimado = esforco_estimado;
+	}
+
+
+	public String getEsforco_real() {
+		return esforco_real;
+	}
+
+
+	public void setEsforco_real(String esforco_real) {
+		this.esforco_real = esforco_real;
+	}
+
+
+	
+
+	
+
+	
+	
 }
