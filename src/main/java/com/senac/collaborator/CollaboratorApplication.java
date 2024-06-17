1993cf38eb949832764d42afeb9c8a5787a5bd9f
digocoutinho78 <digocoutinho@outlook.com>
2024-06-17 14:44:19 -0300

    crud eventReason ok

--- src/main/java/com/senac/collaborator/CollaboratorApplication.java
+++ src/main/java/com/senac/collaborator/CollaboratorApplication.java
@@ -11,27 +11,3 @@ public class CollaboratorApplication {
 	}
 
 }
-
-/*
- * public Boolean deleteEventReason(Long id_event_reason) {
- * Optional<EventReason> eventReasonOptional =
- * eventReasonRepository.findById(id_event_reason); if
- * (eventReasonOptional.isPresent()) { EventReason eventReason =
- * eventReasonOptional.get(); eventReason.setStatus(false);
- * eventReasonRepository.save(eventReason); return true; } return false; }
- */
-/*
- * public boolean updateEventReason(Long id_event_reason, EventReason
- * eventReasonDetails) { Optional<EventReason> eventReasonOptional =
- * eventReasonRepository.findById(id_event_reason); if
- * (eventReasonOptional.isPresent()) { EventReason antigoEventReason =
- * eventReasonOptional.get();
- * antigoEventReason.setIdEventReason(id_event_reason); if
- * (eventReasonDetails.getNome() != null) ; {
- * antigoEventReason.setNome(eventReasonDetails.getNome()); } if
- * (eventReasonDetails.getDescricao() != null) ; {
- * antigoEventReason.setDescricao(eventReasonDetails.getDescricao()); }
- * eventReasonRepository.save(antigoEventReason); return true; } return false;
- * 
- * }
- */
